1fcbead6b4c0dac4b6a86484950ce5a65e574ce9
Atsushi Shibata <shib.ats@gmail.com>
2010-10-22 23:39:23 +0900

    fix information about package

--- setup.py
+++ setup.py
@@ -1,30 +1,62 @@
+"""
+aha
+======
+aha is a web application framework specialized for Google App Engine.
+It provides the finest way to propagate your 'aha !' into cruld :-).
+
+"""
+
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.8a'
+version = '0.81a'
 
 setup(name='aha',
       version=version,
-      description="aha is a weweb application framework specialized for Google App Engine.",
-      long_description="""\
-aha is a web application framework specialized for Google App Engine. It provides the finest way to propagate your 'aha !' into cruld :-).""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='web appengine framework',
-      author='Atsushi Shibata',
-      author_email='shibata@webcore.co.jp',
-      url='http://coreblog.org/aha',
-      license='BSD',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=True,
-      install_requires=[
+      description=("aha is a web application framework specialized for"
+                   " Google App Engine."),
+      long_description = __doc__,
+      keywords= 'web appengine framework',
+      author = 'Atsushi Shibata',
+      author_email = 'shibata@webcore.co.jp',
+      url = 'http://coreblog.org/aha',
+      license = 'BSD',
+      platforms = 'any',
+      include_package_data = True,
+      packages = [
+        'aha',
+        'aha.auth',
+        'aha.controller',
+        'aha.dispatch',
+        'aha.i18n',
+        'aha.model',
+        'aha.modelcontroller',
+        'aha.session',
+        'aha.widget',
+        'aha.wsgi',
+      ],
+      namespace_packages = [
+        'aha',
+        'aha.controller',
+        'aha.widget',
+        'aha.wsgi',
+      ],
+      zip_safe = True,
+      install_requires = [
           # -*- Extra requirements: -*-
           'formencode',
           'mako',
           'baker',
           'werkzeug',
       ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
+      classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
       )
