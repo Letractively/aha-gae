d764d254816e5acff219da17e6d0946b03fffccf
Atsushi Shibata <shib.ats@gmail.com>
2010-10-30 15:51:47 +0900

    fixed test, based on new imprementation of decorator. added test for exposed decorator.

--- aha/controller/tests/test_decorators.py
+++ aha/controller/tests/test_decorators.py
@@ -6,7 +6,7 @@ log = logging.getLogger(__name__)
 
 from nose.tools import *
 
-from coregae.controller.decorators import *
+from aha.controller.decorator import *
 
 class TestDecorators(TestCase):
 
@@ -20,27 +20,56 @@ class TestDecorators(TestCase):
         def t(self):
             return True
 
+        class authclass(object):
+            def auth(arg1, *params, **kws):
+                return True
+
+            def auth_redirect(arg1, *params, **kws):
+                # decorated (controller) class instance is passed
+                #   in first parameter.
+                params[0].redirected = True
+
         class foo(object):
             class request(object):
                 url = 'http://www.example.com'
 
-            @authenticate(t)
+            class response:
+                @staticmethod
+                def set_status(a):
+                    pass
+
+            @authenticate(t, auth_obj=authclass)
             def test_true(self):
                 return "foo"
 
-            @authenticate(f)
+            @authenticate(f, auth_obj=authclass)
             def test_false(self):
                 return "bar"
 
-            def redirect(self, arg):
-                self.redirected = True
-
-            def set_status(self, *args):
-                self.redirected = True
-
 
         i = foo()
+        # decorated method with function that always return true
+        #        must proceed function code
         assert_equal(i.test_true(), 'foo')
+        # decorated method with function that always return false
+        #        must not proceed function code and returns false
         assert_equal(i.test_false(), None)
-        assert_equal(i.redirected, True)
+        # check if authclass.auth_redirect() sets i.redirected = True
+        assert_true(i.redirected)
+
+    def test_expose(self):
+        """
+        Test for expose decorater
+        """
+
+        # dummy function
+        @expose
+        def foo():
+            pass
+
+        # check if foo function has _exposed_ attribute
+        assert_true(hasattr(foo, '_exposed_'))
+        # check if foo,._exposed_ = True
+        assert_true(foo._exposed_)
+
 
