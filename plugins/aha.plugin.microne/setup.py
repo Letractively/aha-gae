dd3b90187fbab8a73d403933ada03846c4e94ae6
Atsushi Shibata <shib.ats@gmail.com>
2011-03-22 12:04:04 +0900

    changed versions.

--- plugins/aha.plugin.microne/setup.py
+++ plugins/aha.plugin.microne/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.6b'
+version = '0.61b'
 
 setup(name='aha.plugin.microne',
       version=version,
--- plugins/aha.plugin.twitteroauth/setup.py
+++ plugins/aha.plugin.twitteroauth/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.91b'
+version = '0.92b'
 
 setup(name='aha.plugin.twitteroauth',
       version=version,
