eda1390a18a80b7c3c32e2ef166e5968fff68704
Atsushi Shibata <shib.ats@gmail.com>
2011-11-07 11:05:02 +0900

    added feature of write expires header

--- applications/aha.application.coreblog3/application/controller/page.py
+++ applications/aha.application.coreblog3/application/controller/page.py
@@ -25,6 +25,7 @@ from aha.modelcontroller.crudcontroller import (ModelCRUDController,
                                                 EditHandler, AddHandler)
 from aha.controller.decorator import authenticate, expose, cache
 
+from blogbase import BlogContentBase
 from model import Page, Path
 config = Config()
 
@@ -94,89 +95,7 @@ class ContentAddHandler(AddHandler):
         controller.render(template = self.FORM_TEMPLATE)
 
 
-class ContentBase(ModelCRUDController):
-    """The base controller for content
-
-    """
-    PAGE_SIZE = 20
-    MODEL = Page
-    FORM_TEMPLATE = '/common/admin/object_form'
-    INDEX_TEMPLATE = '/common/view/page_index'
-
-    @expose
-    @cache()
-    def index(self):
-        obj = self.content
-        t = self.INDEX_TEMPLATE
-        if obj.template:
-            t = obj.template
-        self.render(template = t)
-
-    def set_side_menu_items(self):
-        """
-        A method to obtain menu list.
-            each items in list contains title and link in a tuple.
-            if tuple has 3 items and the third one is sequence,
-                it means menu has sub items.
-        """
-        _ = self.translate
-        c = self.content
-        self.side_menu_items = (
-                ('/style/img/edit_icon.png', 'Edit', c.get_path()+'/edit' ),
-            )
-        if c.get_parent():
-            self.side_menu_items = (
-                ('', 'Going up', c.get_parent().get_path()+'/list' ),
-                )+self.side_menu_items
-
-    edit = expose(authenticate(config.admin_auth)(ContentEditHandler()))
-    add = expose(authenticate(config.admin_auth)(ContentAddHandler()))
-
-    @classmethod
-    def get_form(self, kind, ins = None):
-        """
-        A method to return form object based on given kind.
-        kind must be one of 'add' or 'edit'
-        """
-        from aha.widget.form import Form
-
-        class AddForm(Form):
-            multipart = True
-
-        class EditForm(AddForm):
-            pass
-
-        if kind == 'add':
-            return AddForm()
-        elif kind == 'edit':
-            return EditForm()
-
-    @classmethod
-    def make_add_form(cls, parent = None):
-        """
-        A method to create add form for the content object.
-        You may override this method in your subclass.
-        """
-
-        form = self.get_form('edit', self)
-        form.set_action(cls.BASEPATH+'/add')
-        return form
-
-
-    @classmethod
-    def add_new_object(cls, v, ins):
-        """
-        A method to obtain new object
-        """
-        cu = config.auth_obj().get_user(ins)
-        v['creator'] = cu.get('nickname', '') or cu.get('email', '')
-        d = {}
-        [d.update({str(k): v[k]}) for k in v]
-        o = cls.MODEL(**d)
-        o.put()
-        return o
-
-class PageController(ContentBase):
+class PageController(BlogContentBase):
     """The controller for page
 
     """
